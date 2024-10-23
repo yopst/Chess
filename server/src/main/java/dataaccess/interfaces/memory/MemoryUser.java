deebcb166868a67666398d50b24c2370b6f4fadf
Luc Cole <luculy@icloud.com>
2024-10-22 19:48:13 -0600

    Data Access

--- server/src/main/java/dataaccess/interfaces/memory/MemoryUser.java
+++ server/src/main/java/dataaccess/interfaces/memory/MemoryUser.java
@@ -1,6 +1,37 @@
 package dataaccess.interfaces.memory;
 
+import dataaccess.DataAccessException;
 import dataaccess.interfaces.UserDAO;
+import model.UserData;
 
 public class MemoryUser implements UserDAO {
+    MemoryDatabase db;
+
+    public MemoryUser(MemoryDatabase mdb) {
+        db = mdb;
+    }
+    @Override
+    public void createUser(UserData userData) throws DataAccessException {
+        db.user.put(userData.username(), userData);
+    }
+
+    @Override
+    public UserData getUser(String username) throws DataAccessException {
+        return db.user.get(username);
+    }
+
+    @Override
+    public void deleteUser(String username) throws DataAccessException {
+        db.user.remove(username);
+    }
+
+    @Override
+    public void updateUser(UserData userData) throws DataAccessException {
+        db.user.put(userData.username(), userData);
+    }
+
+    @Override
+    public void clear() throws DataAccessException {
+        db.user.clear();
+    }
 }
