6e7d46b3c47c3eb8608c0c5ce79c2497d1924b65
Luc Cole <luculy@icloud.com>
2024-11-14 19:13:58 -0700

    Start on postLoginRepl

--- client/src/main/java/repl/PostLoginRepl.java
+++ client/src/main/java/repl/PostLoginRepl.java
@@ -4,6 +4,10 @@ import webSocketMessages.Notification;
 import websocket.NotificationHandler;
 
 public class PostLoginRepl extends Repl implements NotificationHandler{
+    public PostLoginRepl(String serverUrl) {
+        super(serverUrl);
+    }
+
     public String help() {
         return """
                 - create <GAMENAME>
