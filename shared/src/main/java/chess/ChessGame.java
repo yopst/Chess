41cd8f311e9ffc2b5f643d854a9f81440c6d2603
Luc Cole <luculy@icloud.com>
2024-12-10 14:36:31 -0700

    switch expression was fine it was the unnamed variable

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -52,28 +52,15 @@ public class ChessGame {
             ChessPiece newPiece;
             TeamColor pieceColor = turn;
             ChessPiece.PieceType type = move.getPromotionPiece();
-            switch (type) {
-                case PAWN:
-                    newPiece = new ChessPiece(pieceColor, ChessPiece.PieceType.PAWN);
-                    break;
-                case ROOK:
-                    newPiece = new ChessPiece(pieceColor, ChessPiece.PieceType.ROOK);
-                    break;
-                case BISHOP:
-                    newPiece = new ChessPiece(pieceColor, ChessPiece.PieceType.BISHOP);
-                    break;
-                case KNIGHT:
-                    newPiece = new ChessPiece(pieceColor, ChessPiece.PieceType.KNIGHT);
-                    break;
-                case QUEEN:
-                    newPiece = new ChessPiece(pieceColor, ChessPiece.PieceType.QUEEN);
-                    break;
-                case KING:
-                    newPiece = new ChessPiece(pieceColor, ChessPiece.PieceType.KING);
-                    break;
-                default:
-                    throw new IllegalArgumentException("Unsupported piece type: " + type);
-            }
+            newPiece = switch (type) {
+                case PAWN -> new ChessPiece(pieceColor, ChessPiece.PieceType.PAWN);
+                case ROOK -> new ChessPiece(pieceColor, ChessPiece.PieceType.ROOK);
+                case BISHOP -> new ChessPiece(pieceColor, ChessPiece.PieceType.BISHOP);
+                case KNIGHT -> new ChessPiece(pieceColor, ChessPiece.PieceType.KNIGHT);
+                case QUEEN -> new ChessPiece(pieceColor, ChessPiece.PieceType.QUEEN);
+                case KING -> new ChessPiece(pieceColor, ChessPiece.PieceType.KING);
+                default -> throw new IllegalArgumentException("Unsupported piece type: " + type);
+            };
             return newPiece;
         }
         return null;
@@ -336,7 +323,7 @@ public class ChessGame {
                             this.undoMove();
                             board.setLastMove(pausedLastMove);
                             return false;
-                        } catch (InvalidMoveException _) { }
+                        } catch (InvalidMoveException e) { continue;}
                     }
                 }
             }
